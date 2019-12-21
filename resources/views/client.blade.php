3785276426740a1df3b06234a1ce0e9ee468aaad
Muhammad Umer <mumeroffice@gmail.com>
2019-12-21 11:57:10 +0500

    lchan

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -10,6 +10,12 @@
 
 </head>
 <body>
+@if(Session::has('Notification_Success'))
+    <div class="alert alert-success alert-dismissible text-center">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+        {!! Session::get('Notification_Success') !!}
+    </div>
+@endif
 <div class="container vh-100">
     <div class="align-content-center d-flex row vh-100">
         <div class="col-md-6 offset-3">
@@ -34,12 +40,7 @@
     </div>
 </div>
 
-@if(Session::has(Notification_Success'))
-    <div class="alert alert-success alert-dismissible text-center">
-        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-        {!! Session::get('Notification_Success') !!}
-    </div>
-@endif
+
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
