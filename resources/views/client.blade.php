1bb38f354cfbdea04900ecd377025293e1bd0677
Muhammad Umer <mumeroffice@gmail.com>
2019-12-21 11:50:59 +0500

    change

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -34,7 +34,7 @@
     </div>
 </div>
 
-@if(Session::has(variable-name'))
+@if(Session::has(Notification_Success'))
     <div class="alert alert-success alert-dismissible text-center">
         <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
         {!! Session::get('Notification_Success') !!}
