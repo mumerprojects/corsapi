4311849cf5bc3425a86a63c0067c29f98eeceada
Muhammad Umer <mumeroffice@gmail.com>
2019-12-21 11:36:41 +0500

    first commit

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    
+	  public function sendMessage($head,$msg){
+		$content = array(
+			"en" => $msg
+            );
+            
+            $headings = array(
+                "en" => $head
+                );
+		
+		$fields = array(
+			'app_id' => "180decf1-cd03-4301-92c9-315e5bc09e01",
+			'included_segments' => array('Active Users'),
+            'data' => array("foo" => "bar"),
+            'headings' => $headings,
+			'contents' => $content
+		);
+		
+		$fields = json_encode($fields);
+    	print("\nJSON sent:\n");
+    	print($fields);
+		
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
+												   'Authorization: Basic MzFkYzhlMDctYTFjNi00YTJlLWFkMmYtOGUyNTRkMDBjMDE1'));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_HEADER, FALSE);
+		curl_setopt($ch, CURLOPT_POST, TRUE);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+		$response = curl_exec($ch);
+		curl_close($ch);
+		
+		return $response;
+	}
+	
+	 public function store(Request $request)
+    {
+    $title = $request->title;
+    $message = $request->message;
+    $response = $this->sendMessage($title,$message);
+	$return["allresponses"] = $response;
+	$return = json_encode( $return);
+	
+	print("\n\nJSON received:\n");
+	print($return);
+	print("\n");   
+    //return ('title is : '.$title.' and msg is : '.$message);
+
+    }
+}
---
+++ resources/views/client.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Push Notification</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<div class="container vh-100">
+    <div class="align-content-center d-flex row vh-100">
+        <div class="col-md-6 offset-3">
+        <div class="card">
+    <div class="card-header bg-success">
+    <h3 class="font-weight-bold text-center text-white">Broadcast Push Notification</h3>
+    </div>
+    <div class="card-body">
+    <form action="{{url('/clients')}}" method='post'> 
+        @csrf
+        <input class="form-control" name="title" placeholder="Message Title" type="text" required>
+        <br>
+        <textarea class="form-control" rows="5" name="message" placeholder="Message Body" type="text" required></textarea>
+        <br>
+    </div>
+    <div class="card-footer text-center">
+    <button class="btn btn-success rounded-pill px-5" type='submit'>Send Notification</button>
+        </form>
+    </div>
+</div>
+        </div>
+    </div>
+</div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+</body>
+</html>
